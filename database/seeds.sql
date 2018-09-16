5f7b402c9d7fb9b3584fb0ac713b9516105c4025
tnandate <nandate.takuya@moneyforward.co.jp>
2018-09-16 21:22:43 +0900

    create posts table

--- database/seeds.sql
+++ database/seeds.sql
@@ -1,2 +1,5 @@
 INSERT INTO topics (title, created_at, updated_at) VALUES ('スレタイ1', NOW(), NOW());
 INSERT INTO topics (title, created_at, updated_at) VALUES ('スレタイ2', NOW(), NOW());
+
+INSERT INTO posts (name, body, topic_id, created_at, updated_at ) VALUES ('名無しさん', 'hoge', 1, NOW(), NOW());
+INSERT INTO posts (name, body, topic_id, created_at, updated_at ) VALUES ('名無しさん', 'fuga', 2, NOW(), NOW());
